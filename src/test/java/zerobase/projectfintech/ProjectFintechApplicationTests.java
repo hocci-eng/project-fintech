ed6718311d1f59b51ae7ca0e685793d4f351e8df
Jeon Jaeyong <gndk1026@gmail.com>
2024-12-19 21:32:33 +0900

    프로젝트 설정

---
+++ src/test/java/zerobase/projectfintech/ProjectFintechApplicationTests.java
@@ -0,0 +1,13 @@
+package zerobase.projectfintech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectFintechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
